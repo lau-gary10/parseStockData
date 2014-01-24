17eb9856070d07bcba329d2df095c235c0491972
jetwater5 <lau.gary10@gmail.com>
2014-01-23 19:05:45 -0500

    v0.1
    Added MIT License

--- download_fundamental_data.py
+++ download_fundamental_data.py
@@ -27,6 +27,30 @@ http://financials.morningstar.com/ajax/ReportProcess4CSV.html?&t=XASE:ZBB&region
 
 Last stock price for ZBB (check this website for additional info - http://www.gummy-stuff.org/Yahoo-data.htm)
 http://finance.yahoo.com/d/quotes.csv?s=ZBB&f=l1
+
+#########################################################################
+The MIT License (MIT)
+
+Copyright (c) 2014 Gary Lau
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#########################################################################
 '''
 
 import urllib.request
--- index.py
+++ index.py
@@ -7,6 +7,30 @@ This runs (in order):
 
 This file calls:
     common_lib.py
+
+#########################################################################
+The MIT License (MIT)
+
+Copyright (c) 2014 Gary Lau
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#########################################################################
 '''
 
 # Runs get_tickers.py with error checking
@@ -66,23 +90,25 @@ def partial_run(tickerFilename, lastPriceFlag):
     return
 
 # Asks user for full run or partial run. Returns boolean
-def ask_user_full_run_or_partial_run():
-    print("Full run or partial run?")
-    print("Type '1' for partial run. Type '2' for full run.")
+def ask_if_user_want_to_download_list_of_tickers():
+    print("Would you like to download a list of tickers first? (Y/N)")
+    print("If yes, type 'Y'. If no, type 'N'.")
+    print("Downloading a list of tickers will extend running time. Only select this option if you must.")
     boolAnswer = input("Answer: ")
-    if boolAnswer == '1':
-        print("User selected partial run.")
+    boolAnswer  = boolAnswer.lower()
+    if boolAnswer == 'y':
+        print("User selected 'Y'.")
         boolAnswer = True
-    elif boolAnswer == '2':
-        print("User selected full run.")
+    elif boolAnswer == 'n':
+        print("User selected 'N'")
         boolAnswer = False
     else:
-        print("Unknown user input. Defaulting to partial run.")
+        print("Unknown user input. Defaulting to 'N'.")
         boolAnswer = True
     return boolAnswer
 
 def main():
-    userTypeRun = ask_user_full_run_or_partial_run()
+    userTypeRun = ask_if_user_want_to_download_list_of_tickers()
     from common_lib import check_which_exchange_user_wants_to_get_info_from, check_if_user_wants_to_download_last_price_only
     tickerFilename = check_which_exchange_user_wants_to_get_info_from()
     lastPriceFlag = check_if_user_wants_to_download_last_price_only()
